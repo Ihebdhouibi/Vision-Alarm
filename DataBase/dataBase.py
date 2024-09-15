f7e378207ed5a6b5030e2290aa3b94ec72738ba2
Ihebdhouibi <ihebdhouibidh@gmail.com>
2024-09-15 21:52:37 +0100

    Review- Apply code reusebility to retrieving alerts functions

--- DataBase/dataBase.py
+++ DataBase/dataBase.py
@@ -47,6 +47,7 @@ def remove_camera(id):
     pass
 
 def store_alert_data(alert_time, video_link, alert_class, alert_type):
+    
     query = f"INSERT INTO {alert_type}_alerts (alert_time, video_link, class) VALUES (%s, %s, %s)"
 
     with get_db_connection() as (cursor, conn):
@@ -56,45 +57,23 @@ def store_alert_data(alert_time, video_link, alert_class, alert_type):
         except Exception as e:
             print(f"Error inserting alert into {alert_type}_alerts: {e}")
 
-def retrieve_fire_alerts():
+def retrieve_alerts(alert_type):
+    
+    query=f"SELECT * FROM {alert_type}_alerts"
 
-    # Establishing Connection to DB
     with get_db_connection() as (cursor, conn):
-        query = "select * from fire_alerts"
         cursor.execute(query)
-        print("Fire alerts : \n ------------------------------------ \n")
-        fire_alerts = cursor.fetchall()
-        for row in fire_alerts:
-            print(f"ID : {row[0]} | alert time : {row[1]} | video link : {row[2]} | class : {row[3]}")
-
-
-def retrieve_fall_alerts():
-
-    # Establishing Connection to DB
-    with get_db_connection() as (cursor, conn):
-        query = "select * from fall_alerts"
-        cursor.execute(query)
-        print("Fall alerts : \n ------------------------------------ \n")
-        fall_alerts = cursor.fetchall()
-        for row in fall_alerts:
-            print(f"ID : {row[0]} | alert time : {row[1]} | video link : {row[2]} | class : {row[3]}")
-
-def retrieve_robbery_alerts():
-
-    # Establishing Connection to DB
-    with get_db_connection() as (cursor, conn):
-        query = "select * from robbery_alerts"
-        cursor.execute(query)
-        print("Robbery alerts : \n ------------------------------------ \n")
-        robbery_alerts = cursor.fetchall()
-        for row in robbery_alerts:
-            print(f"ID : {row[0]} | alert time : {row[1]} | video link : {row[2]} | class : {row[3]}")
+        alerts = cursor.fetchall()
+        print(f"{alert_type.capitalize()} alerts: \n ----------------------------------------------- \n")
+        
+        for row in alerts:
+            print(f"ID: {row[0]} | Alert Time: {row[1]} | Video Link; {row[2]} | Class: {row[3]}")
 
 def retrieve_all_alerts():
 
-    retrieve_fire_alerts()
-    retrieve_fall_alerts()
-    retrieve_robbery_alerts()
+    retrieve_alerts("fire")
+    retrieve_alerts("fall")
+    retrieve_alerts("robbery")
     
 def retrieve_users():
 
