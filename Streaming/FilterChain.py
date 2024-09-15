244dcb24e0d4c8503a9187108f597b3d0cefd2ae
Ihebdhouibi <ihebdhouibidh@gmail.com>
2024-09-16 00:12:13 +0100

    Review- removing debbuging and unecessary lines

--- Streaming/FilterChain.py
+++ Streaming/FilterChain.py
@@ -5,161 +5,6 @@ from valkka.api2.tools import parameterInitCheck, typeCheck
 from valkka.api2 import FragMP4ShmemClient
 from valkka.api2.logging import setValkkaLogLevel, loglevel_silent
 
-
-# class BasicFilterchain:
-#     """This class implements the following filterchain:
-#
-#     ::
-#
-#       (LiveThread:livethread) -->> (AVThread:avthread) -->> (OpenGLThread:glthread)
-#
-#     i.e. the stream is decoded by an AVThread and sent to the OpenGLThread for presentation
-#     """
-#     setValkkaLogLevel(loglevel_silent)
-#     parameter_defs = {
-#         "livethread": LiveThread,
-#         "openglthread": OpenGLThread,
-#         "address": str,
-#         "slot": int,
-#
-#         # these are for the AVThread instance:
-#         "n_basic": (int, 20),  # number of payload frames in the stack
-#         "n_setup": (int, 20),  # number of setup frames in the stack
-#         "n_signal": (int, 20),  # number of signal frames in the stack
-#         "flush_when_full": (bool, False),  # clear fifo at overflow
-#
-#         "affinity": (int, -1),
-#         "verbose": (bool, False),
-#         "msreconnect": (int, 0),
-#
-#         # Timestamp correction type: TimeCorrectionType_none,
-#         # TimeCorrectionType_dummy, or TimeCorrectionType_smart (default)
-#         "time_correction": None,
-#         # Operating system socket ringbuffer size in bytes # 0 means default
-#         "recv_buffer_size": (int, 0),
-#         # Reordering buffer time for Live555 packets in MILLIseconds # 0 means
-#         # default
-#         "reordering_mstime": (int, 0),
-#         "n_threads": (int, 1)
-#     }
-#
-#     def __init__(self, **kwargs):
-#         # auxiliary string for debugging output
-#         self.pre = self.__class__.__name__ + " : "
-#         # check for input parameters, attach them to this instance as
-#         # attributes
-#         parameterInitCheck(self.parameter_defs, kwargs, self)
-#         self.init()
-#
-#     def init(self):
-#         self.idst = str(id(self))
-#         self.makeChain()
-#         self.createContext()
-#         self.startThreads()
-#         self.active = True
-#
-#     def __del__(self):
-#         self.close()
-#
-#     def close(self):
-#         if (self.active):
-#             if (self.verbose):
-#                 print(self.pre, "Closing threads and contexes")
-#             self.decodingOff()
-#             self.closeContext()
-#             self.stopThreads()
-#             self.active = False
-#
-#     def makeChain(self):
-#         """Create the filter chain
-#         """
-#         self.gl_in_filter = self.openglthread.getInput(
-#         )  # get input FrameFilter from OpenGLThread
-#
-#         self.framefifo_ctx = core.FrameFifoContext()
-#         self.framefifo_ctx.n_basic = self.n_basic
-#         self.framefifo_ctx.n_setup = self.n_setup
-#         self.framefifo_ctx.n_signal = self.n_signal
-#         self.framefifo_ctx.flush_when_full = self.flush_when_full
-#
-#         self.avthread = core.AVThread(
-#             "avthread_" + self.idst,
-#             self.gl_in_filter,
-#             self.framefifo_ctx)
-#
-#         if self.affinity > -1 and self.n_threads > 1:
-#             print("WARNING: can't use affinity with multiple threads")
-#
-#         self.avthread.setAffinity(self.affinity)
-#         if self.affinity > -1:
-#             self.avthread.setNumberOfThreads(self.n_threads)
-#
-#         # get input FrameFilter from AVThread
-#         self.av_in_filter = self.avthread.getFrameFilter()
-#
-#     def createContext(self):
-#         """Creates a LiveConnectionContext and registers it to LiveThread
-#         """
-#         # define stream source, how the stream is passed on, etc.
-#
-#         self.ctx = core.LiveConnectionContext()
-#         # slot number identifies the stream source
-#         self.ctx.slot = self.slot
-#
-#         if (self.address.find("rtsp://") == 0):
-#             self.ctx.connection_type = core.LiveConnectionType_rtsp
-#         else:
-#             self.ctx.connection_type = core.LiveConnectionType_sdp  # this is an rtsp connection
-#
-#         self.ctx.address = self.address
-#         # stream address, i.e. "rtsp://.."
-#
-#         self.ctx.framefilter = self.av_in_filter
-#
-#         self.ctx.msreconnect = self.msreconnect
-#
-#         # some extra parameters
-#         """
-#     // ctx.time_correction =TimeCorrectionType::none;
-#     // ctx.time_correction =TimeCorrectionType::dummy;
-#     // default time correction is smart
-#     // ctx.recv_buffer_size=1024*1024*2;  // Operating system ringbuffer size for incoming socket
-#     // ctx.reordering_time =100000;       // Live555 packet reordering treshold time (microsecs)
-#     """
-#         if (self.time_correction is not None):
-#             self.ctx.time_correction = self.time_correction
-#         # self.time_correction=core.TimeCorrectionType_smart # default ..
-#         self.ctx.recv_buffer_size = self.recv_buffer_size
-#         self.ctx.reordering_time = self.reordering_mstime * \       1000  # from millisecs to microsecs
-#
-#         # send the information about the stream to LiveThread
-#         self.livethread.registerStream(self.ctx)
-#         self.livethread.playStream(self.ctx)
-#
-#
-#     def closeContext(self):
-#         self.livethread.stopStream(self.ctx)
-#         self.livethread.deregisterStream(self.ctx)
-#
-#     def startThreads(self):
-#         """Starts thread required by the filter chain
-#         """
-#         self.avthread.startCall()
-#
-#     def stopThreads(self):
-#         """Stops threads in the filter chain
-#         """
-#         self.avthread.stopCall()
-#
-#     def decodingOff(self):
-#         self.avthread.decodingOffCall()
-#
-#     def decodingOn(self):
-#         self.avthread.decodingOnCall()
-
-
-
-
 class VisionAlarmFilterChain:
     """A filter chain with a shared mem hook and FragMP4ShmemFrameFilter
 
@@ -298,9 +143,7 @@ class VisionAlarmFilterChain:
         # self.av_in_filter1_1 = self.avthread1_1.getFrameFilter()
 
         # Branch 2 : Saving frames to shared memory for openCV/Tensorflow process
-        # these two lines for debugging bullshit so feel free to comment/uncomment them ya man
-        print(self.pre, "using shmem name ", self.shmem_name)
-        print(self.shmem_name)
+        
         try:
             self.shmem_filter = core.RGBShmemFrameFilter(
                 self.shmem_name,
@@ -345,9 +188,7 @@ class VisionAlarmFilterChain:
             self.interval_filter
 
         )
-        se
-
-
+ 
         # Main branch
         self.framefifo_ctx = core.FrameFifoContext()
         self.framefifo_ctx.n_basic = self.n_basic
